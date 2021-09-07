68bc435dd895e0870c14291d1ea23d1ff024c7c8
Wilgen Garcia <wilgengarcia2001@gmail.com>
2021-09-06 21:40:31 -0500

    app terminada

---
+++ js/app.js
@@ -0,0 +1,60 @@
+
+// window.onload = function() {
+//     let loading = document.querySelector('.loading')
+//     loading.setAttribute('class', 'dnone')
+//     window.scroll(0,0)
+// };
+
+const side = document.getElementById('side')
+const btnMenu = document.getElementById('btnMenu')
+const iconoMenu = document.getElementById('iconoMenu')
+const nombre = document.getElementById('nombre')
+const descripcion = document.getElementById('descripcion')
+const enviarComentario = document.getElementById('enviarComentario')
+const  modal = document.getElementById('modal')
+const cerrarModal = document.getElementById('cerrarModal')
+const tituloModal = document.getElementById('tituloModal')
+const parrafoModal = document.getElementById('parrafoModal')
+
+btnMenu.addEventListener('click', () => {
+    side.classList.toggle('menu-colapsed')
+    iconoMenu.classList.toggle('fa-bars')
+    iconoMenu.classList.toggle('fa-times')
+})
+
+enviarComentario.addEventListener('click', async ()=>{
+    if(!nombre.value || !descripcion.value){
+        modal.setAttribute('class', 'modal-container')
+        tituloModal.innerHTML=`Error!! 😣`
+        parrafoModal.innerHTML=`Todos los campos son Obligatorios`
+        return
+    }else{
+        tituloModal.innerHTML=`Gracias por tu comentario!!! 😁`
+        parrafoModal.innerHTML=``
+    }
+    const db = firebase.firestore();
+    enviarComentario.innerHTML=`<i class="fas fa-sync"></i>`
+    
+    try {
+        await db.collection('mensajes').doc(nombre.value).set({
+            nombre: nombre.value,
+            descripcion: descripcion.value
+        })
+        modal.setAttribute('class', 'modal-container')
+    } catch (error) {
+        modal.setAttribute('class', 'modal-container')
+        tituloModal.innerHTML=`Error Inesperado!! 😣`
+        parrafoModal.innerHTML=`No se pudo guardar tu comentario, intentalo mas tarde`
+    }
+    enviarComentario.innerHTML=`Enviar`
+})
+
+cerrarModal.addEventListener('click', ()=>{
+    let modal = document.getElementById('modal')
+    modal.setAttribute('class', 'modal-container none')
+})
+
+
+
+
+    
---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCjY1fUtnj8zSDn-edr60KmU41ZhmGksLI",
+    authDomain: "paginawilgen-d75c3.firebaseapp.com",
+    projectId: "paginawilgen-d75c3",
+    storageBucket: "paginawilgen-d75c3.appspot.com",
+    messagingSenderId: "521203487506",
+    appId: "1:521203487506:web:8596f02669d99f2d9019cb",
+    measurementId: "G-JG6J2CKD95"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
